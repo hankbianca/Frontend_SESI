d94cf9edf1a81539c0b45b970e3ba357d2ee6810
hankbianca <biancahank2020@gmail.com>
2026-05-15 12:15:48 -0300

    Continuação Frontend JS

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
 function executarSistema() {
     const nome = document.getElementById("inputNome").value;
+    const idade = parseInt (document.getElementById("inputNome").value);
+    const valor = parseFloat(document.getElementById("inputValor").value);
+    const cupom = document.getElementById("inputCupom").value === "true";
+
+// Dados de saída
+const sg = document.getElementById("mensagem-autorizacao");
+const lista = document.getElementById("lista-estoque");
+const relatorio = document.getElementById("relatorio-final");
+
+// Validação para campos vazios
+if(!nome || isNaN(idade) || isNaN(valor)) {
+    alert("Por favor, preencha todos os dados!");
+    return;
+}
+
+ // Regra de negócio
+ if (idade >= 16) {
+    msg.innerText = `Venda autorizada: ${nome}`;
+    msg.style.color = "#00ff88";
+    
+    // Desconto
+    let valorFinal = (valor > 500 || cupom) ? valor * 0.85 : valor;
+
+    // Estoque
+    let estoque = ["Placa de Vídeo", "Processdor", "Memória RAM"];
+    lista.innerHTML = ""; // Limpa a lista anterior
+}
 }
